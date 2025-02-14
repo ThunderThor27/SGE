62bd866a0d83aefa4e6175a645e1c6f88f1ba612
ThunderThor27 <tiagocrepaldisalomao@gmail.com>
2025-02-14 11:27:06 -0300

    Entrega da atividade

---
+++ Models/Etapas.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SGE.Models
+{
+    [Table("Etapas")]
+    public class Etapas
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("titulo")]
+        [Display(Name = "Etapa")]
+        public string Titulo { get; set; } = string.Empty;
+
+        [Column("ano")]
+        [Display(Name = "Ano da Etapa")]
+        public int Ano { get; set; }
+    }
+}
